387c453d40cde34af6add65e4c29897135666ce6
SamihaD <samiha.dr@gmail.com>
2023-09-07 20:55:50 +0200

    add main class

---
+++ backend/src/main/java/epfc/eu/pickADesk/PickADeskApplication.java
@@ -0,0 +1,13 @@
+package epfc.eu.pickADesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickADeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickADeskApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/epfc/eu/pickADesk/PickADeskApplicationTests.java
@@ -0,0 +1,13 @@
+package epfc.eu.pickADesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickADeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
