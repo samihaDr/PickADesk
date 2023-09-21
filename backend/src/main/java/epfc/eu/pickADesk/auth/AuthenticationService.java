7fe6c205c0d96a11cbaa4c7bb8d769b15540928a
SamihaD <samiha.dr@gmail.com>
2023-09-21 13:55:44 +0200

    AuthenticationService : add class with register method

---
+++ backend/src/main/java/epfc/eu/pickADesk/auth/AuthenticationService.java
@@ -0,0 +1,38 @@
+package epfc.eu.pickADesk.auth;
+
+import epfc.eu.pickADesk.config.JwtService;
+import epfc.eu.pickADesk.user.Role;
+import epfc.eu.pickADesk.user.User;
+import epfc.eu.pickADesk.user.UserRepository;
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+
+}
+
