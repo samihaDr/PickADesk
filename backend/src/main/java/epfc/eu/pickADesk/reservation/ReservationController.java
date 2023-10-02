9f6fb2b8fd2915f404be8a90f891a155f08d05d4
SamihaD <samiha.dr@gmail.com>
2023-10-02 12:36:06 +0200

    ReservationController: implement addReservation method to ReservationController

--- backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationController.java
+++ backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationController.java
@@ -1,5 +1,6 @@
 package epfc.eu.pickADesk.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,12 @@ import java.util.List;
 @RequestMapping("/api/reservations")
 public class ReservationController {
 
+    private final ReservationService reservationService;
 
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
     @GetMapping(value="/myReservations")
     public ResponseEntity<List<Reservation>> listReservations() {
         Reservation reservation1 = new Reservation();
@@ -31,5 +37,14 @@ public class ReservationController {
         return new ResponseEntity<>(List.of(reservation1, reservation2), HttpStatus.OK);
     }
 
+    @PostMapping(value="/addReservation")
+    public ResponseEntity<Reservation> addReservation(@RequestBody Reservation reservation) {
+        // s'assurer que le champ "id" est nul, car il devrait être généré automatiquement
 
+        // validez et traiter les données de réservation si nécessaire
+
+        // appeler le service pour ajouter la réservation
+
+        return ResponseEntity.ok(reservationService.addReservation(reservation));
+    }
 }
