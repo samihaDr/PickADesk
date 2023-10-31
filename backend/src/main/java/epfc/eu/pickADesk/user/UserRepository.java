2d3a48c963cb0f2b1d1a2e38a186a2e99bba85b8
SamihaD <samiha.dr@gmail.com>
2023-10-31 22:07:41 +0100

    Refactor(UserRepository) : add Optional attribute to findOneById method

--- backend/src/main/java/epfc/eu/pickADesk/user/UserRepository.java
+++ backend/src/main/java/epfc/eu/pickADesk/user/UserRepository.java
@@ -6,8 +6,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByEmail(String email);
-
-    User findOneById(Long id);
+    Optional<User> findOneById(Long id);
 
 }
 
