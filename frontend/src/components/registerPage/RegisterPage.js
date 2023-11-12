930b3e3c065cc8668daa441a09c12e346dbda73f
SamihaD <samiha.dr@gmail.com>
2023-11-12 16:19:55 +0100

    Feat(RegisterPage) : add email and password validation and refactor code

--- frontend/src/components/registerPage/RegisterPage.js
+++ frontend/src/components/registerPage/RegisterPage.js
@@ -1,8 +1,53 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import "./RegisterPage.scss";
+import { AUTH_TOKEN_KEY } from "../../App";
+import axios from "axios";
+import { EmailValidator } from "../../services/EmailValidator";
+import { PasswordValidator } from "../../services/PasswordValidator";
 
 export default function RegisterPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    lastname: "",
+    firstname: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (
+      EmailValidator(formData.email) &&
+      PasswordValidator(formData.password)
+    ) {
+      axios
+        .post("/api/auth/register", formData)
+        .then((response) => {
+          const jwt = response.data.token;
+          sessionStorage.setItem(AUTH_TOKEN_KEY, jwt);
+        })
+        .catch((error) => {
+          if (error.response) {
+            // La requête a reçu une réponse du serveur, vous pouvez accéder au statut HTTP ici
+            console.error("Erreur HTTP :", error.response.status);
+          } else if (error.request) {
+            // La requête a été faite, mais aucune réponse n'a été reçue (peut être dû à un problème de connexion)
+            console.error(
+              "La requête a été faite mais aucune réponse n'a été reçue.",
+            );
+          } else {
+            // Une erreur inattendue s'est produite
+            console.error("Erreur inattendue :", error.message);
+          }
+        });
+      setFormData({ email: "", lastname: "", firstname: "", password: "" });
+    }
+  };
   return (
     <div>
       <card className="card">
@@ -10,7 +55,7 @@ export default function RegisterPage() {
         <div className="register-container">
           <div>
             <div className="form-container">
-              <form>
+              <form onSubmit={onSubmit}>
                 <div className="mb-3">
                   <label htmlFor="InputEmail" className="form-label">
                     Email address
@@ -18,7 +63,10 @@ export default function RegisterPage() {
                   <input
                     type="email"
                     className="form-control"
-                    id="InputEmail"
+                    id="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
                   />
 
                   <div className="mb-3">
@@ -28,7 +76,10 @@ export default function RegisterPage() {
                     <input
                       type="text"
                       className="form-control"
-                      id="InputLastname"
+                      id="Lastname"
+                      name="lastname"
+                      value={formData.lastname}
+                      onChange={handleChange}
                       aria-describedby="lastnameHelp"
                     />
                     <div id="lastnameHelp" className="form-text">
@@ -42,7 +93,10 @@ export default function RegisterPage() {
                     <input
                       type="text"
                       className="form-control"
-                      id="InputFirstname"
+                      id="Firstname"
+                      name="firstname"
+                      value={formData.firstname}
+                      onChange={handleChange}
                       aria-describedby="firstnameHelp"
                     />
                     <div id="firstnameHelp" className="form-text">
@@ -57,24 +111,28 @@ export default function RegisterPage() {
                       type="password"
                       className="form-control"
                       id="Password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
                       aria-describedby="passwordHelp"
                     />
                     <div id="passwordHelp" className="form-text">
-                      enter a password containing at least 6 letters, a number
+                      enter a password containing at least 8 letters, a number
                       and a special character.
                     </div>
                   </div>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="ConfirmPassword" className="form-label">
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="ConfirmPassword"
-                  />
-                </div>
+                {/*<div className="mb-3">*/}
+                {/*  <label htmlFor="ConfirmPassword" className="form-label">*/}
+                {/*    Confirm Password*/}
+                {/*  </label>*/}
+                {/*  <input*/}
+                {/*    type="password"*/}
+                {/*    className="form-control"*/}
+                {/*    id="ConfirmPassword"*/}
+                {/*    onChange={handleChange}*/}
+                {/*  />*/}
+                {/*</div>*/}
                 <button type="submit" className="btn btn-primary">
                   Register
                 </button>
