07ea72557946e7d5a476f3b17ff7ea6d5c693b10
SamihaD <samiha.dr@gmail.com>
2023-11-12 16:21:24 +0100

    Refactor(RegisterRequest, PasswordValidator)

--- backend/src/main/java/epfc/eu/pickADesk/auth/RegisterRequest.java
+++ backend/src/main/java/epfc/eu/pickADesk/auth/RegisterRequest.java
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-    private String firstname;
-    private String lastname;
     private String email;
+    private String lastname;
+    private String firstname;
     private String password;
 }
