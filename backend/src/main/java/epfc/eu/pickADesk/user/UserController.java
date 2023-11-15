0f1498ee5b4147e9882dcbc1ce3c346b83ef0e2f
SamihaD <samiha.dr@gmail.com>
2023-11-15 23:09:55 +0100

    Feat(userController): add and implement getUsers and logout

---
+++ backend/src/main/java/epfc/eu/pickADesk/user/UserController.java
@@ -0,0 +1,46 @@
+package epfc.eu.pickADesk.user;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping("/api/users")
+//@Hidden
+public class UserController {
+
+    private final UserService userService;
+    private final LogoutService logoutService;
+
+    @Autowired
+    public UserController(UserService userService, LogoutService logoutService) {
+        this.userService = userService;
+        this.logoutService = logoutService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<?> getUsers(Principal principal) {
+        if (!this.userService.getUserConnectedRole(principal)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        logoutService.logout(request, response, authentication);
+        return ResponseEntity.accepted().build();
+    }
+}
