24698517929bab82e8a99e1fd4a4569a7aaf69fe
SamihaD <samiha.dr@gmail.com>
2023-11-18 02:48:32 +0100

    Feat(User) : add UserDTO and UserMapper

---
+++ backend/src/main/java/epfc/eu/pickADesk/user/UserDTO.java
@@ -0,0 +1,32 @@
+package epfc.eu.pickADesk.user;
+
+import epfc.eu.pickADesk.reservation.Reservation;
+import lombok.*;
+
+import java.util.List;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String lastname;
+    private String firstname;
+    private List<Reservation> reservationList;
+    private Role role;
+    private Boolean locked;
+    private Boolean enabled;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.lastname = user.getLastname();
+        this.firstname = user.getFirstname();
+        this.reservationList = user.getReservationList();
+        this.role = user.getRole();
+        this.enabled = user.getEnabled();
+        this.locked = user.getLocked();
+    }
+}
+
---
+++ backend/src/main/java/epfc/eu/pickADesk/user/UserMapper.java
@@ -0,0 +1,26 @@
+package epfc.eu.pickADesk.user;
+
+import epfc.eu.pickADesk.reservation.ReservationMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper(componentModel = "spring", uses = ReservationMapper.class)
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+//    @Mapping(target = "password", ignore = true)
+    UserDTO userToUserDTO(Optional<User> user);
+
+    @Mapping(target = "id", ignore = true)
+    User userDTOToUser(UserDTO userDTO);
+
+    List<UserDTO> userListToUserDTOList(List<User> userList);
+    default UserDTO map(User value) {
+        return userToUserDTO(Optional.ofNullable(value));
+    }
+
+}
