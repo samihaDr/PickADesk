cb0b2b5c061a9ab3c2e4680b614a4710937295b0
SamihaD <samiha.dr@gmail.com>
2023-11-18 02:43:36 +0100

    Refactor : change the name of method

--- backend/src/main/java/epfc/eu/pickADesk/config/ApplicationConfig.java
+++ backend/src/main/java/epfc/eu/pickADesk/config/ApplicationConfig.java
@@ -20,7 +20,7 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> userRepository.findOneByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
     }
 
