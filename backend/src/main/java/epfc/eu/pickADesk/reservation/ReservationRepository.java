f70ad259a392a96956b9d0408f9e04d12569fb1b
SamihaD <samiha.dr@gmail.com>
2023-11-21 16:28:03 +0100

    Feat(display today's reservation) : add methode to retrieve today's reservation

--- backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationRepository.java
+++ backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationRepository.java
@@ -2,6 +2,7 @@ package epfc.eu.pickADesk.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,4 +10,7 @@ import java.util.Optional;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     //Optional<Reservation> findById(Long reservationId);
     List<Reservation> findByUserId(Long userId);
+
+    Optional <Reservation> findReservationsByUserIdAndReservationDate(Long userId, LocalDate reservationDate);
+
 }
