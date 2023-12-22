9e9bf610b6aa295db2166bf22229da4fb4b62699
SamihaD <samiha.dr@gmail.com>
2023-12-22 23:31:20 +0100

    fix(Reservation) : modify ManyToOne Annotation

--- backend/src/main/java/epfc/eu/pickADesk/reservation/Reservation.java
+++ backend/src/main/java/epfc/eu/pickADesk/reservation/Reservation.java
@@ -1,6 +1,7 @@
 package epfc.eu.pickADesk.reservation;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import epfc.eu.pickADesk.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,9 +26,9 @@ public class Reservation {
     private Integer nbTimeSlot;
     private Integer workStationId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id") // Cette colonne stocke la clé étrangère pour l'utilisateur associé à cette réservation.
-    //@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    // Cette colonne stocke la clé étrangère pour l'utilisateur associé à cette réservation.
     @JsonBackReference
     private User user;
     private Integer reservationTypeId;
