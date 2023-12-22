d913d995bfe472de5abceffa4d7c16f17e1da36a
SamihaD <samiha.dr@gmail.com>
2023-12-23 00:02:12 +0100

    fix(home) : fix and adjust view

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -4,28 +4,30 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <div>
-      <card className="card">
-        <h2 style={{ color: "#1f4e5f" }}> Welcome</h2>
-        <div className="home-container">
-          <div className="card-body">
-            <div className="card-img">
-              <img src={logo} alt="Logo" width="300" height="250" />
-            </div>
-            <h3 className="card-text" style={{ color: "#1f4e5f" }}>
-              Book easily. Work efficiently
-            </h3>
-            <div className="card-links">
-              <a href="/LoginPage" className="card-link">
-                Login
-              </a>
-              <a href="/RegisterPage" className="card-link">
-                Register
-              </a>
-            </div>
+    <div className="main">
+      <h2 style={{ color: "#1f4e5f" }}> Welcome</h2>
+      <div className="home-container">
+        <div className="card-body">
+          <div className="card-img">
+            <img src={logo} alt="Logo" width="300" height="250" />
+          </div>
+          <h3 className="card-text" style={{ color: "#1f4e5f" }}>
+            Book easily. Work efficiently
+          </h3>
+          <div className="card-links">
+            <a href="/LoginPage" className="link">
+              Login
+            </a>
+            {/* Ajout d'un espace entre les liens */}
+            <span className="link-space">
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            </span>
+            <a href="/RegisterPage" className="link">
+              Register
+            </a>
           </div>
         </div>
-      </card>
+      </div>
     </div>
   );
 }
