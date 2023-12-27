df2d75e5407b01c4bc43a545c2e15e99dd29c169
SamihaD <samiha.dr@gmail.com>
2023-12-27 18:10:02 +0100

    feat(Frontend: ProfilePage) : WIP use the different component (PersonalInfo, LocationInfo, and BookingPreferences) in the profilePage view

--- frontend/src/components/profilePage/ProfilePage.js
+++ frontend/src/components/profilePage/ProfilePage.js
@@ -2,17 +2,25 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { GlobalContext } from "../../services/GlobalState";
 import { getUserConnected } from "../../services/getUserConnected";
-import { useNavigate } from "react-router-dom";
+import "./ProfilePage.scss";
+import PersonalInfo from "./PersonalInfo";
+import LocationInfo from "./LocationInfo";
+import BookingPreferences from "./BookingPreferences";
 
 const EditProfile = () => {
   const { userConnected, setUserConnected } = useContext(GlobalContext);
   console.log("UserConnected in Profile : " + userConnected);
-  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     lastname: "",
     firstname: "",
     department: "",
     team: "",
+    country: "",
+    city: "",
+    office: "",
+    address: "",
+    floor: "",
   });
 
   const [error, setError] = useState("");
@@ -79,12 +87,12 @@ const EditProfile = () => {
     fetchUserData();
   }, []);
 
-  const handleChangePasswordClick = () => {
-    navigate("/changePassword"); // Redirige vers la page de changement de mot de passe
-  };
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+  };
+  const updatePreference = (value) => {
+    // ... La logique pour mettre à jour les préférences ...
   };
 
   const onSubmit = async (event) => {
@@ -108,79 +116,14 @@ const EditProfile = () => {
 
   return (
     <div className="main">
-      <h2 style={{ color: "#1f4e5f" }}>Edit profile</h2>
-      <div className="editProfile-container">
-        <div>
-          <div className="form-container">
-            <form onSubmit={onSubmit}>
-              <div className="mb-3">
-                <label htmlFor="InputLastname" className="form-label">
-                  LastName
-                </label>
-                <input
-                  disabled={true}
-                  type="text"
-                  className="form-control"
-                  id="Lastname"
-                  name="lastname"
-                  value={formData.lastname}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="InputFirstname" className="form-label">
-                  FirstName
-                </label>
-                <input
-                  disabled={true}
-                  type="text"
-                  className="form-control"
-                  id="Firstname"
-                  name="firstname"
-                  value={formData.firstname}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="InputDepartment" className="form-label">
-                  Department
-                </label>
-                <input
-                  disabled={true}
-                  type="text"
-                  className="form-control"
-                  id="Department"
-                  name="department"
-                  value={formData.department}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="InputTeam" className="form-label">
-                  Team
-                </label>
-                <input
-                  disabled={true}
-                  type="text"
-                  className="form-control"
-                  id="Team"
-                  name="team"
-                  value={formData.team}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mb-3">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={handleChangePasswordClick}
-                >
-                  Change Password
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+      <h2>Edit profile</h2>
+      <div className="accordion" id="accordionExample">
+        <PersonalInfo formData={formData} handleChange={handleChange} />
+        <LocationInfo formData={formData} handleChange={handleChange} />
+        <BookingPreferences
+          formData={formData}
+          updatePreference={updatePreference}
+        />
       </div>
     </div>
   );
