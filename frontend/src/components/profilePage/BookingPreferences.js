6ab8e1fe607810fa5960f743b575bb84de8544d6
SamihaD <samiha.dr@gmail.com>
2023-12-31 15:41:09 +0100

    feat(Front: BookingPreferences) : implement Booking preferences component

--- frontend/src/components/profilePage/BookingPreferences.js
+++ frontend/src/components/profilePage/BookingPreferences.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const BookingPreferences = ({ formData, updatePreference }) => {
+  const handleCheckboxChange = (field, value) => {
+    const updatedValues = formData[field].includes(value)
+      ? formData[field].filter((item) => item !== value)
+      : [...formData[field], value];
+    updatePreference(field, updatedValues);
+  };
   return (
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingThree">
@@ -22,25 +28,110 @@ const BookingPreferences = ({ formData, updatePreference }) => {
         data-bs-parent="#accordionExample"
       >
         <div className="accordion-body">
-          <strong>This is the third item's accordion body.</strong> It is hidden
-          by default, until the collapse plugin adds the appropriate classes
-          that we use to style each element. These classes control the overall
-          appearance, as well as the showing and hiding via CSS transitions. You
-          can modify any of this with custom CSS or overriding our default
-          variables. It's also worth noting that just about any HTML can go
-          within the <code>.accordion-body</code>, though the transition does
-          limit overflow.
+          <div className="mb-3">
+            <label>Reservation type</label>
+            <select
+              name="reservationTypeId"
+              onChange={(e) => updatePreference(e.target.name, e.target.value)}
+              className="form-select"
+              value={formData.reservationTypeId}
+            >
+              <option value="">Select a Reservation Type</option>
+              {formData.reservationTypes &&
+                formData.reservationTypes.map((reservationType) => (
+                  <option key={reservationType.id} value={reservationType.id}>
+                    {reservationType.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label>Work Area</label>
+            <select
+              name="workAreaId"
+              onChange={(e) => updatePreference(e.target.name, e.target.value)}
+              className="form-select"
+              value={formData.workAreaId}
+            >
+              <option value="">Select a Work area</option>
+              {formData.workAreas &&
+                formData.workAreas.map((workArea) => (
+                  <option key={workArea.id} value={workArea.id}>
+                    {workArea.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label>Screen</label>
+            <select
+              name="screenId"
+              onChange={(e) => updatePreference(e.target.name, e.target.value)}
+              className="form-select"
+              value={formData.screenId}
+            >
+              <option value="">Select a screen</option>
+              {formData.screens &&
+                formData.screens.map((screen) => (
+                  <option key={screen.id} value={screen.id}>
+                    {screen.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label>Equipments</label>
+            {formData.equipments &&
+              formData.equipments.map((equipment) => (
+                <div key={equipment.id} className="form-check">
+                  <input
+                    type="checkbox"
+                    id={`equipment-${equipment.id}`}
+                    name="equipmentIds"
+                    value={equipment.id}
+                    checked={formData.equipmentIds?.includes(equipment.id)}
+                    onChange={() =>
+                      handleCheckboxChange("equipmentIds", equipment.id)
+                    }
+                    className="form-check-input"
+                  />
+                  <label
+                    htmlFor={`equipment-${equipment.id}`}
+                    className="form-check-label"
+                  >
+                    {equipment.name}
+                  </label>
+                </div>
+              ))}
+          </div>
+
+          <div className="mb-3">
+            <label>Furniture</label>
+            {formData.furnitures &&
+              formData.furnitures.map((furniture) => (
+                <div key={furniture.id} className="form-check">
+                  <input
+                    type="checkbox"
+                    id={`furniture-${furniture.id}`}
+                    name="furnitureIds"
+                    value={furniture.id}
+                    checked={formData.furnitureIds?.includes(furniture.id)}
+                    onChange={() =>
+                      handleCheckboxChange("furnitureIds", furniture.id)
+                    }
+                    className="form-check-input"
+                  />
+                  <label
+                    htmlFor={`furniture-${furniture.id}`}
+                    className="form-check-label"
+                  >
+                    {furniture.name}
+                  </label>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-
-      <input
-        type="text"
-        className="form-control"
-        id="Preference"
-        name="preference"
-        value={formData.preference}
-        onChange={(e) => updatePreference(e.target.value)}
-      />
     </div>
   );
 };
