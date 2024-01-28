62ee02867f90a45d07472cafc817fa5e53fedd3c
SamihaD <samiha.dr@gmail.com>
2024-01-28 22:38:01 +0100

    Feat (searchWorkStations) : add a validateFormData method and a spinner for laoading

--- frontend/src/components/searchWorkStation/SearchWorkStation.js
+++ frontend/src/components/searchWorkStation/SearchWorkStation.js
@@ -4,6 +4,9 @@ import { getBookingPreferencesData } from "../../services/GetBookingPreferencesD
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"; // CSS import
 import "./SearchWorkStation.scss";
+import { useNavigate } from "react-router-dom";
+import { WorkStationContext } from "../../services/WorkStationContext";
+
 export default function SearchWorkStation() {
   const [data, setData] = useState({
     reservationTypes: [],
@@ -13,7 +16,11 @@ export default function SearchWorkStation() {
     furniture: [],
   });
 
-  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const { workStations, setWorkStations, setSelectedOptions } =
+    useContext(WorkStationContext);
+  const [isLoading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
   const { userConnected, userPreferences } = useContext(GlobalContext);
   const [date, setDate] = useState(new Date());
   const today = new Date();
@@ -32,11 +39,13 @@ export default function SearchWorkStation() {
   const isDateSelectable = (date) => {
     return date >= minDate;
   };
+
   const [timePeriod, setTimePeriod] = useState({
     morning: false,
     afternoon: false,
   });
 
+  const [zone, setZone] = useState("");
   const [reservationType, setReservationType] = useState("");
   const [workArea, setWorkArea] = useState("");
   const [screen, setScreen] = useState("");
@@ -46,8 +55,12 @@ export default function SearchWorkStation() {
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
-      const bookingPreferencesData = await getBookingPreferencesData();
-      setData(bookingPreferencesData);
+      try {
+        const bookingPreferencesData = await getBookingPreferencesData();
+        setData(bookingPreferencesData);
+      } catch (error) {
+        console.error("Error fetching booking preferences:", error);
+      }
       setLoading(false);
     }
 
@@ -56,6 +69,7 @@ export default function SearchWorkStation() {
 
   useEffect(() => {
     if (userPreferences) {
+      setZone(userPreferences.zone?.id || "");
       setReservationType(userPreferences.reservationType?.id || "");
       setWorkArea(userPreferences.workArea?.id || "");
       setScreen(userPreferences.screen?.id || "");
@@ -64,28 +78,111 @@ export default function SearchWorkStation() {
     }
   }, [userPreferences]);
 
-  const handleSubmit = (e) => {
+  const validateFormData = () => {
+    // Vérifier que la date est sélectionnée et valide
+    if (!date) {
+      return "The date is compulsory.";
+    }
+
+    // Vérifier que la date sélectionnée n'est pas dans le passé
+    if (date <= today) {
+      return "The date selected cannot be in the past.";
+    }
+
+    // Vérifier que soit morning, soit afternoon, soit les deux sont cochés
+    if (!timePeriod.morning && !timePeriod.afternoon) {
+      return "Please select at least one period: Morning or Afternoon.";
+    }
+    const validationResult = "";
+    console.log("Validation result:", validationResult);
+    return validationResult;
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Logic to handle form submission
-    console.log({
+    console.log("handleSubmit called");
+    const validationError = validateFormData();
+    if (validationError) {
+      setErrorMessage(validationError);
+      return;
+    }
+    setErrorMessage("");
+    setLoading(true);
+
+    const formData = {
       startDate: date,
       timePeriod,
+      zone,
       reservationType,
       workArea,
       screen,
       equipment,
       furniture,
-    });
+    };
+
+    try {
+      const response = await sendFormDataToServer(formData);
+      console.log("Response from Backend : ", response);
+      setWorkStations(response);
+      console.log("Available Work Stations : ", workStations);
+      setSelectedOptions({
+        zone,
+        date,
+        timePeriod,
+        reservationType,
+        workArea,
+      });
+      navigate("/availableWorkStations");
+    } catch (error) {
+      console.error("Error submitting form", error);
+    } finally {
+      setLoading(false);
+    }
   };
-  console.log("MON OBJET IN SEARCH ...... : ", {
-    startDate: date,
-    timePeriod,
-    reservationType,
-    workArea,
-    screen,
-    equipment,
-    furniture,
-  });
+
+  const sendFormDataToServer = async (formData) => {
+    const {
+      //startDate,  // Note: Vous devrez peut-être adapter ce champ en fonction de la façon dont votre backend gère les dates
+      //timePeriod, // Note: Ce champ n'est pas directement utilisé dans le backend, à moins qu'il ne corresponde à 'zoneId'
+      zone,
+      workArea,
+      screen,
+      equipment,
+      furniture,
+    } = formData;
+
+    const params = new URLSearchParams({
+      zoneId: zone,
+      workAreaId: workArea,
+      screenId: screen,
+      // Convertir les tableaux en chaînes de requête, par exemple: "1,2,3"
+      equipmentIds: equipment.join(","),
+      furnitureIds: furniture.join(","),
+      // Paramètres de pagination par défaut, modifiez-les si nécessaire
+      page: 0,
+      size: 10,
+      sortBy: "id",
+      order: "asc",
+    });
+
+    try {
+      const response = await fetch(`/api/workStations/search?${params}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching data", error);
+      throw error;
+    }
+  };
+
   const handleTimePeriodCheckboxChange = (e) => {
     setTimePeriod({
       ...timePeriod,
@@ -103,9 +200,18 @@ export default function SearchWorkStation() {
     isEquipment ? setEquipment(newItems) : setFurniture(newItems);
   };
   if (!userConnected) {
-    return null; // Ne rien rendre si l'utilisateur n'est pas connecté
+    return null;
   }
 
+  if (isLoading) {
+    return (
+      <div className="text-center">
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <div className="main">
@@ -158,6 +264,7 @@ export default function SearchWorkStation() {
                     onChange={(e) => setReservationType(e.target.value)}
                     className="form-select"
                   >
+                    <option value="">Any</option>
                     {data.reservationTypes.map((type) => (
                       <option key={type.id} value={type.id}>
                         {type.name}
@@ -174,6 +281,7 @@ export default function SearchWorkStation() {
                     aria-label="Default select example"
                     name="workAreaId"
                   >
+                    <option value="">Any</option>
                     {data.workAreas.map((area) => (
                       <option key={area.id} value={area.id}>
                         {area.name}
@@ -190,6 +298,7 @@ export default function SearchWorkStation() {
                     aria-label="Default select example"
                     name="screenId"
                   >
+                    <option value="">Any</option>
                     {data.screens.map((screen) => (
                       <option key={screen.id} value={screen.id}>
                         {screen.name}
@@ -226,6 +335,7 @@ export default function SearchWorkStation() {
                           onChange={() =>
                             handleCheckboxChange(furn.id, "furniture")
                           }
+                          className="form-check-input"
                         />{" "}
                         {furn.name}
                       </div>
@@ -234,10 +344,14 @@ export default function SearchWorkStation() {
                 </div>
                 <div className="container-submit">
                   <button type="submit" className="btn btn-primary">
-                    {" "}
                     Search
                   </button>
                 </div>
+                {errorMessage && (
+                  <div style={{ color: "red" }} className="error-message">
+                    {errorMessage}
+                  </div>
+                )}
               </form>
             </div>
           </div>
