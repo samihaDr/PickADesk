612b884116399ffa3d84848c238e14d3e9a63963
SamihaD <samiha.dr@gmail.com>
2024-02-02 22:44:52 +0100

    Fix (Home) : modify the Css file, adapt and clean up the code

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -5,26 +5,24 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div className="main">
-      <h2 style={{ color: "#1f4e5f" }}> Welcome</h2>
+      <h2> Welcome</h2>
       <div className="home-container">
         <div className="card-body">
           <div className="card-img">
             <img src={logo} alt="Logo" width="300" height="250" />
           </div>
-          <h3 className="card-text" style={{ color: "#1f4e5f" }}>
-            Book easily. Work efficiently
-          </h3>
+          <h3 className="card-text">Book easily. Work efficiently</h3>
           <div className="card-links">
             <a href="/LoginPage" className="link">
               Login
             </a>
-            {/* Ajout d'un espace entre les liens */}
-            <span className="link-space">
-              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-            </span>
-            <a href="/RegisterPage" className="link">
-              Register
-            </a>
+            {/*/!* Ajout d'un espace entre les liens *!/*/}
+            {/*<span className="link-space">*/}
+            {/*  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;*/}
+            {/*</span>*/}
+            {/*<a href="/RegisterPage" className="link">*/}
+            {/*  Register*/}
+            {/*</a>*/}
           </div>
         </div>
       </div>
