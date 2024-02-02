7ec90edf96f1892586c558ac235e0360f255eca3
SamihaD <samiha.dr@gmail.com>
2024-02-02 22:41:30 +0100

    Fix (ChangePassword) : modify the Css file, adapt and clean up the code

--- frontend/src/components/changePassword/ChangePassword.js
+++ frontend/src/components/changePassword/ChangePassword.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { PasswordValidator } from "../../services/PasswordValidator";
 import { useNavigate } from "react-router-dom";
+import "./ChangePassword.scss";
 
 const ChangePasswordForm = () => {
   const [passwordData, setPasswordData] = useState({
