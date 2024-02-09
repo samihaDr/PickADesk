a87b3ab0d31dad8422d5908b9a1466f4e7cdf6df
SamihaD <samiha.dr@gmail.com>
2024-02-09 13:23:46 +0100

    Feat(PickAReservation) : remove findById method and optionnal annotation from ReservationRepository

--- backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationRepository.java
+++ backend/src/main/java/epfc/eu/pickADesk/reservation/ReservationRepository.java
@@ -3,17 +3,12 @@ package epfc.eu.pickADesk.reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    @NonNull
-    Optional<Reservation> findById(Long reservationId);
 
     List<Reservation> findByUserId(Long userId);
 
