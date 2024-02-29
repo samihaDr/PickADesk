8d8d8e7cf7106ca3e7624c2216bc06e4761c80c9
SamihaD <samiha.dr@gmail.com>
2024-02-29 18:17:08 +0100

    feat(GlobalState) : enable navigation using the address bar, by adding the new variables isAutenticated, userInfo

--- frontend/src/services/GlobalState.js
+++ frontend/src/services/GlobalState.js
@@ -1,10 +1,14 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
+
+const AUTH_TOKEN_KEY = "jhi-authenticationToken";
 
 // Initialiser l'état global
 const initialState = {
   userConnected: null,
+  userInfo: null,
   userPreferences: null,
   weeklyQuota: null,
+  isAuthenticated: !!sessionStorage.getItem(AUTH_TOKEN_KEY),
 };
 
 export const GlobalContext = createContext(initialState);
@@ -13,10 +17,16 @@ export const GlobalContext = createContext(initialState);
 const globalReducer = (state, action) => {
   console.log("Action dispatched:", action);
   switch (action.type) {
-    case "SET_USER":
+    case "SET_USER_CONNECTED":
       return {
         ...state,
         userConnected: action.payload,
+      };
+    case "SET_USER_INFO":
+      return {
+        ...state,
+        userInfo: action.payload,
+        isAuthenticated: !!action.payload, // isAuthenticated est vrai si payload n'est pas null
       };
     case "SET_USER_PREFERENCES":
       return {
@@ -27,6 +37,11 @@ const globalReducer = (state, action) => {
       return {
         ...state,
         weeklyQuota: action.payload,
+      };
+    case "SET_AUTHENTICATED":
+      return {
+        ...state,
+        isAuthenticated: !!action.payload,
       };
     default:
       return state;
@@ -39,21 +54,62 @@ export const GlobalProvider = ({ children }) => {
 
   // Actions pour modifier l'état
   const setUserConnected = (user) => {
-    dispatch({ type: "SET_USER", payload: user });
+    dispatch({ type: "SET_USER_CONNECTED", payload: user });
+  };
+  const setUserInfo = (user) => {
+    sessionStorage.setItem("userInfo", JSON.stringify(user)); // Persister l'utilisateur connecté dans localStorage
+    dispatch({ type: "SET_USER_INFO", payload: user });
   };
   const setUserPreferences = (userPreferences) => {
+    sessionStorage.setItem("userPreferences", JSON.stringify(userPreferences));
     dispatch({
       type: "SET_USER_PREFERENCES",
       payload: userPreferences,
     });
   };
   const setWeeklyQuota = (quota) => {
+    sessionStorage.setItem("weeklyQuota", JSON.stringify(quota));
     dispatch({ type: "SET_QUOTA", payload: quota });
   };
+  const setIsAuthenticated = (isAuthenticated) => {
+    dispatch({ type: "SET_AUTHENTICATED", payload: isAuthenticated });
+    // Mise à jour du stockage du navigateur pour refléter l'état d'authentification
+    if (isAuthenticated) {
+      sessionStorage.setItem("isAuthenticated", isAuthenticated);
+    } else {
+      // En cas de déconnexion, supprimez le jeton du stockage
+      sessionStorage.removeItem(AUTH_TOKEN_KEY);
+    }
+  };
+  // Restaurer l'état de connexion de l'utilisateur au démarrage de l'application
+  useEffect(() => {
+    const storedUserInfo = sessionStorage.getItem("userInfo");
+    if (storedUserInfo) {
+      const user = JSON.parse(storedUserInfo);
+      dispatch({ type: "SET_USER_INFO", payload: user });
+    }
+    const storedUserPreferences = sessionStorage.getItem("userPreferences");
+    if (storedUserPreferences) {
+      const userPreferences = JSON.parse(storedUserPreferences);
+      dispatch({ type: "SET_USER_PREFERENCES", payload: userPreferences });
+    }
+    const storedQuota = sessionStorage.getItem("weeklyQuota");
+    if (storedQuota) {
+      const quota = JSON.parse(storedQuota);
+      dispatch({ type: "SET_QUOTA", payload: quota });
+    }
+  }, []);
 
   return (
     <GlobalContext.Provider
-      value={{ ...state, setUserConnected, setWeeklyQuota, setUserPreferences }}
+      value={{
+        ...state,
+        setUserConnected,
+        setUserInfo,
+        setWeeklyQuota,
+        setUserPreferences,
+        setIsAuthenticated,
+      }}
     >
       {children}
     </GlobalContext.Provider>
