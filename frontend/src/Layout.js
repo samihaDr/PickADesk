9f220ebe7fddac8850bcc5a77ee40d7583bd4e39
SamihaD <samiha.dr@gmail.com>
2024-02-29 19:01:32 +0100

    Refactor(Layout) : use isAuthenticated variable instead of UserConnected to manage the display of the navbar and menu components

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import { GlobalContext } from "./services/GlobalState";
 import Navbar from "./components/navbar/Navbar";
 import Menu from "./components/menu/Menu";
-
 const Layout = ({ children }) => {
-  const { userConnected } = useContext(GlobalContext);
+  const { isAuthenticated } = useContext(GlobalContext);
 
   return (
     <>
-      {userConnected && <Navbar />}
-      {userConnected && <Menu />}
+      {isAuthenticated && <Navbar />}
+      {isAuthenticated && <Menu />}
       {children}
     </>
   );
