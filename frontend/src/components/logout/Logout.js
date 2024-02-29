e19fde4df409e12fa7a8290e96b5dc5571d0e10a
SamihaD <samiha.dr@gmail.com>
2024-02-29 19:11:16 +0100

    Feat(Logout) : remove all items in SessionStorage and refactor code

--- frontend/src/components/logout/Logout.js
+++ frontend/src/components/logout/Logout.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../../services/GlobalState";
 
 export const AUTH_TOKEN_KEY = "jhi-authenticationToken";
 
-const LogoutButton = ({ setUserConnected, setUserPreferences }) => {
+const LogoutButton = () => {
   const navigate = useNavigate();
-
+  const {
+    setUserConnected,
+    setUserPreferences,
+    setIsAuthenticated,
+    setUserInfo,
+    setWeeklyQuota,
+  } = useContext(GlobalContext);
   const logout = async () => {
     try {
       const authToken = sessionStorage.getItem(AUTH_TOKEN_KEY);
@@ -14,12 +22,20 @@ const LogoutButton = ({ setUserConnected, setUserPreferences }) => {
         const headers = { Authorization: `Bearer ${authToken}` };
 
         await axios.post("/api/users/logout", null, { headers });
-
-        sessionStorage.removeItem(AUTH_TOKEN_KEY);
-
+        setIsAuthenticated(false);
         setUserPreferences([]);
         setUserConnected(null);
-        navigate("/login");
+        setUserInfo([]);
+        setWeeklyQuota(null);
+        sessionStorage.removeItem(AUTH_TOKEN_KEY);
+        sessionStorage.removeItem("weeklyQuota");
+        sessionStorage.removeItem("userInfo");
+        sessionStorage.removeItem("userPreferences");
+        sessionStorage.removeItem("isAuthenticated");
+
+        // Redirection et rechargement de la page
+        navigate("/login", { replace: true });
+        window.location.reload();
       }
     } catch (error) {
       console.error("Erreur lors de la déconnexion :", error);
