dddecb9518e93327467065e42e9a0a126f329b4e
SamihaD <samiha.dr@gmail.com>
2024-04-23 23:31:11 +0200

    Feat(WeeklyTeamStats) : add and implement WeeklyTeamStats

--- frontend/src/components/myTeam/TeamSettings.js
+++ frontend/src/components/myTeam/TeamSettings.js
@@ -1,208 +1,237 @@
-import { useContext, useEffect, useState } from "react";
-import { GlobalContext } from "../../services/GlobalState";
+import {useContext, useEffect, useState} from "react";
+import {GlobalContext} from "../../services/GlobalState";
 import axios from "axios";
-import { AUTH_TOKEN_KEY } from "../../App";
+import {AUTH_TOKEN_KEY} from "../../App";
 import "./TeamSettings.scss"
+
 export default function TeamSettings() {
-  const { userInfo, weeklyRemaining, weeklyQuota,  } = useContext(GlobalContext);
-  const [teamList, setTeamList] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-  const [teamId, setTeamId] = useState("");
-  const [shouldUpdateStatuses, setShouldUpdateStatuses] = useState(false);
+    let {userInfo} = useContext(GlobalContext);
+    const [teamList, setTeamList] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const jwt = sessionStorage.getItem(AUTH_TOKEN_KEY);
 
-  const jwt = sessionStorage.getItem(AUTH_TOKEN_KEY);
+    useEffect(() => {
+        if (userInfo && userInfo.teamId) {
+            fetchTeamList(userInfo.teamId);
+        }
+    }, [userInfo]);
 
-  useEffect(() => {
-    if (userInfo) {
-      setTeamId(userInfo.teamId);
+    async function fetchTeamList(teamId) {
+        setLoading(true);
+        try {
+            const response = await axios.get(`/api/users/getTeamList/${teamId}`, {
+                headers: {Authorization: `Bearer ${jwt}`},
+            });
+            if (Array.isArray(response.data)) {
+                updateMembersWithDetails(response.data);
+            } else {
+                throw new Error("Data is not an array");
+            }
+        } catch (error) {
+            console.error("Fetch team list error:", error);
+            setError("Unable to load the list of employees: " + error.message);
+        } finally {
+            setLoading(false);
+        }
     }
-  }, [userInfo]);
 
-  useEffect(() => {
-    console.log("TeamID Updated: ", teamId);
-    fetchTeamList();
-  }, [teamId]);
-
-  useEffect(() => {
-    const fetchStatuses = async () => {
-      const statusPromises = teamList.map(member =>
-          checkColleagueAvailability(member.id)
-      );
-      const statuses = await Promise.all(statusPromises);
-      const updatedTeamList = teamList.map((member, index) => ({
-        ...member,
-        ...statuses[index],
-      }));
-      setTeamList(updatedTeamList);
-      setShouldUpdateStatuses(false);
-    };
-
-    if (shouldUpdateStatuses) {
-      fetchStatuses();
+    async function updateMembersWithDetails(members) {
+        const membersWithDetails = await Promise.all(members.map(async member => {
+            const availability = await checkColleagueAvailability(member.id);
+            const weeklyReservations = await employeeWeeklyReservations(member.id);
+            const daysWorked = getDaysPerWeek(member.workSchedule);
+            return {
+                ...member,
+                ...availability,
+                weeklyReservations,
+                daysWorked,
+                teleworkingDays: weeklyReservations,
+                daysInOffice: daysWorked - weeklyReservations
+            };
+        }));
+        setTeamList(membersWithDetails);
     }
-  }, [shouldUpdateStatuses]);
 
-  async function fetchTeamList() {
-    if (!teamId) return;
-    setLoading(true);
-    try {
-      const response = await axios.get(`/api/users/getTeamList/${teamId}`, {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      });
-      console.log("API Response 1:", response.data);
-      if (Array.isArray(response.data)) {
-        const sortedData = response.data
-            // .filter((member) => member.id !== userInfo.id)
-            .sort((b, a) => a.lastname.localeCompare(b.lastname));
-        setTeamList(sortedData);
-        setShouldUpdateStatuses(true);  // Trigger status updates only after team list is updated
-      }
-    } catch (error) {
-      console.error("Fetch team list error:", error);
-      setError("Unable to load the list of employees.");
-    } finally {
-      setLoading(false);
+    async function checkColleagueAvailability(employeeId) {
+        try {
+            const url = `/api/reservations/employeeHasReservationToday/${employeeId}`;
+            const response = await axios.get(url, {
+                headers: {Authorization: `Bearer ${jwt}`},
+            });
+            return response.data.success ? {
+                workStatus: "In Office",
+                period: response.data.data[0]?.morning ? "Morning" : "Afternoon",
+                deskNumber: response.data.data[0]?.workStation.workPlace
+            } : {
+                workStatus: "Teleworking",
+                period: null,
+                deskNumber: null
+            };
+        } catch (error) {
+            console.error("Error checking reservation:", error);
+            return {
+                workStatus: "Error checking status",
+                period: null,
+                deskNumber: null
+            };
+        }
     }
-  }
 
-  async function checkColleagueAvailability(employeeId) {
-    try {
-      const url = `/api/reservations/employeeHasReservationToday/${employeeId}`;
-      const response = await axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      });
-      console.log("API Response2:", response.data);
-      if (response.data.success && response.data.data.length > 0) {
-        const reservation = response.data.data[0];  // Suppose using only the first reservation
-        return {
-          workStatus: "In Office",
-          period: reservation.morning ? "Morning" : "Afternoon",
-          deskNumber: reservation.workStation.workPlace
+    async function employeeWeeklyReservations(employeeId) {
+        try {
+            const url = `/api/reservations/getReservationsForWeek/${employeeId}`;
+            const response = await axios.get(url, {
+                headers: {Authorization: `Bearer ${jwt}`},
+            });
+            let totalDaysReserved = 0;
+
+            if (response.data.reservations) {
+                response.data.reservations.forEach((reservation) => {
+                    if (reservation.morning && reservation.afternoon) {
+                        totalDaysReserved += 1;
+                    } else if (reservation.morning || reservation.afternoon) {
+                        totalDaysReserved += 0.5;
+                    }
+                });
+            }
+            return totalDaysReserved;
+        } catch (error) {
+            console.error("Error fetching weekly reservations:", error);
+            return 0;
+        }
+    }
+
+    // Calcul des pourcentages
+    const totalCount = teamList.length;
+    const inOfficeCount = teamList.filter(member => member.workStatus === 'In Office').length;
+    const teleworkingCount = teamList.filter(member => member.workStatus === 'Teleworking').length;
+    const inOfficePercent = ((inOfficeCount / totalCount) * 100).toFixed(0);
+    const teleworkingPercent = ((teleworkingCount / totalCount) * 100).toFixed(0);
+
+    function getDaysPerWeek(schedule) {
+        const scheduleDays = {
+            "FULL_TIME": 5,
+            "HALF_TIME": 2.5,
+            "FOUR_FIFTHS": 4
         };
-      } else {
-        return {
-          workStatus: "Teleworking",
-          period: null,
-          deskNumber: null
-        };
-      }
-    } catch (error) {
-      console.error("Error checking reservation:", error);
-      return {
-        workStatus: "Error checking status",
-        period: null,
-        deskNumber: null
-      };
+        return scheduleDays[schedule] || 0;
     }
-  }
 
-  // Calcul des pourcentages
-  const totalCount = teamList.length;
-  const inOfficeCount = teamList.filter(member => member.workStatus === 'In Office').length;
-  const teleworkingCount = teamList.filter(member => member.workStatus === 'Teleworking').length;
-  const inOfficePercent = ((inOfficeCount / totalCount) * 100).toFixed(0);
-  const teleworkingPercent = ((teleworkingCount / totalCount) * 100).toFixed(0);
+    function getPersonalStats() {
+        return teamList.find(member => member.id === userInfo.id);
+    }
 
-  //Calcul des stats de l'utilisateur connecté
+    const isManager = userInfo.role === "MANAGER";
 
-  const teleworkingDays = weeklyQuota - weeklyRemaining;
-  const daysInOffice = getDaysPerWeek(userInfo.workSchedule) - teleworkingDays;
-  function getDaysPerWeek(schedule) {
-    const scheduleDays = {
-      "FULL_TIME": 5,
-      "HALF_TIME": 2.5,
-      "FOUR_FIFTHS": 4
-    };
-    return scheduleDays[schedule] || 0; // Renvoie 0 si la valeur n'est pas trouvée, ce qui pourrait indiquer une erreur de données
-  }
+    return (
+        <div className="main">
+            <h2>My Team</h2>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                <div className="result">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>WorkStatus</th>
+                            <th>Session</th>
+                            <th>WorkPlace</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {teamList.map((member, index) => (
+                            <tr key={index}>
+                                <td>{member.firstname}</td>
+                                <td>{member.lastname}</td>
+                                <td>{member.workStatus}</td>
+                                <td>{member.period}</td>
+                                <td>{member.deskNumber}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+            <h2>Daily Team Statistics </h2>
+            <div className="progress-bars">
+                <div>
+                    <label><strong> In Office </strong></label>
 
-  return (
-      <div className="main">
-        <h2>Team static</h2>
-        <div className="progress-bars">
-          <div>
-            <label><strong>In Office </strong></label>
-
-            <div className="progress">
-              <div className="progress-bar" role="progressbar" style={{ width: `${inOfficePercent}%`, backgroundColor: '#007bff' }}  aria-valuemin="0" aria-valuemax="100">{inOfficePercent}%</div>
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             style={{width: `${inOfficePercent}%`, backgroundColor: '#007bff'}} aria-valuemin="0"
+                             aria-valuemax="100">{inOfficePercent}%
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <label><strong>Teleworking</strong> </label>
+                    <div className="progress">
+                        <div className="progress-bar" role="progressbar"
+                             style={{width: `${teleworkingPercent}%`, backgroundColor: '#28a745'}} aria-valuemin="0"
+                             aria-valuemax="100">{teleworkingPercent}%
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div>
-            <label><strong>Teleworking</strong> </label>
-            <div className="progress">
-              <div className="progress-bar" role="progressbar" style={{ width: `${teleworkingPercent}%`, backgroundColor: '#28a745' }}  aria-valuemin="0" aria-valuemax="100">{teleworkingPercent}%</div>
-            </div>
-          </div>
+
+
+            {isManager ? (
+                <div className="stats">
+                    <h2> Weekly Team Statistics</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Days Worked</th>
+                            <th>Days Teleworking</th>
+                            <th>Days In Office</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {teamList.map((member, index) => (
+                            <tr key={index}>
+                                <td>{member.firstname}</td>
+                                <td>{member.lastname}</td>
+                                <td>{getDaysPerWeek(member.workSchedule)} days</td>
+                                <td>{member.teleworkingDays}</td>
+                                <td>{member.daysInOffice}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <div className="stats">
+                    <h2>My Personal Statistics</h2>
+                    {getPersonalStats() ? (
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Days Worked</th>
+                                <th>Days Teleworking</th>
+                                <th>Days In Office</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{getPersonalStats().daysWorked}</td>
+                                <td>{getPersonalStats().teleworkingDays}</td>
+                                <td>{getPersonalStats().daysInOffice}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    ) : (
+                        <p>No personal statistics available.</p>
+                    )}
+                </div>
+            )}
         </div>
-
-        <h2>My team</h2>
-        {isLoading ? (
-            <p>Loading...</p>
-        ) : error ? (
-            <p>{error}</p>
-        ) : (
-            <div className="result">
-              <table>
-                <thead>
-                <tr>
-                  <th>Firstname</th>
-                  <th>Lastname</th>
-                  <th>WorkStatus</th>
-                  <th>Session</th>
-                  <th>WorkPlace</th>
-                </tr>
-                </thead>
-                <tbody>
-                {teamList.map((member, index) => (
-                    <tr key={index}>
-                      <td>{member.firstname}</td>
-                      <td>{member.lastname}</td>
-                      <td>{member.workStatus}</td>
-                      <td>{member.period}</td>
-                      <td>{member.deskNumber}</td>
-                    </tr>
-                ))}
-                </tbody>
-              </table>
-            </div>
-        )}
-
-        <h2>My static</h2>
-        {isLoading ? (
-            <p>Loading...</p>
-        ) : error ? (
-            <p>{error}</p>
-        ) : (
-            <div className="stats">
-              <table>
-                <thead>
-                <tr>
-                  <th>Firstname</th>
-                  <th>Lastname</th>
-                  <th>Days Worked</th>
-                  <th>Days teleworking</th>
-                  <th>Days in Office</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                    <tr>
-                      <td>{userInfo.firstname}</td>
-                      <td>{userInfo.lastname}</td>
-                      <td>{getDaysPerWeek(userInfo.workSchedule)} days</td>
-                      <td>{teleworkingDays}</td>
-                      <td>{daysInOffice}</td>
-                    </tr>
-
-                </tbody>
-              </table>
-            </div>
-        )}
-      </div>
-  );
+    );
 }
