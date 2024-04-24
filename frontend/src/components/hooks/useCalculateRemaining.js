3649e2987aeb034796d96dc3e65e26b0367e43c8
SamihaD <samiha.dr@gmail.com>
2024-04-24 22:58:08 +0200

    fix(useCalculateRemaining) : set the weeklyRemaining with the memberQuota when the employee has no bookings

--- frontend/src/components/hooks/useCalculateRemaining.js
+++ frontend/src/components/hooks/useCalculateRemaining.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 
 const useCalculateRemaining = () => {
-  const { setWeeklyRemaining } = useContext(GlobalContext);
+  const { userInfo, setWeeklyRemaining } = useContext(GlobalContext);
 
   return useCallback(
     async (userId) => {
@@ -23,18 +23,23 @@ const useCalculateRemaining = () => {
           },
         );
         console.log("UseCalculateRemaining : ", response.data.reservations);
-        let totalDaysReserved = 0;
-        response.data.reservations.forEach((reservation) => {
-          if (reservation.morning && reservation.afternoon) {
-            totalDaysReserved += 1;
-          } else if (reservation.morning || reservation.afternoon) {
-            totalDaysReserved += 0.5;
-          }
-        });
-        const memberQuota = response.data.memberQuota;
-        console.log("MemberQuota : ", memberQuota);
-        const newRemaining = memberQuota - totalDaysReserved;
-        setWeeklyRemaining(newRemaining);
+        if (response.data.reservations ){
+            let totalDaysReserved = 0;
+            response.data.reservations.forEach((reservation) => {
+                if (reservation.morning && reservation.afternoon) {
+                    totalDaysReserved += 1;
+                } else if (reservation.morning || reservation.afternoon) {
+                    totalDaysReserved += 0.5;
+                }
+            });
+            const memberQuota = response.data.memberQuota;
+            console.log("MemberQuota : ", memberQuota);
+            const newRemaining = memberQuota - totalDaysReserved;
+            setWeeklyRemaining(newRemaining);
+        } else {
+            setWeeklyRemaining(userInfo.memberQuota);
+        }
+
       } catch (error) {
         console.error("Failed to fetch reservations:", error);
       }
