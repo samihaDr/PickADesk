5a4775fa0c33ae27cd4bc65a3ad8208b4b5635f5
SamihaD <samiha.dr@gmail.com>
2024-05-06 23:29:34 +0200

    Feat(add group reservation BookWorkStation) : add and implement a service to add GroupReservation

--- frontend/src/services/BookWorkStation.js
+++ frontend/src/services/BookWorkStation.js
@@ -1,44 +1,29 @@
 import axios from "axios";
 import { AUTH_TOKEN_KEY } from "../App";
-import { format } from "date-fns";
-import { getUserConnected } from "./GetUserConnected";
 
-export const bookWorkStation = async (stationId, selectedOptions) => {
-  const isoDateString = selectedOptions.date;
-  const date = new Date(isoDateString);
-  const formattedDate = format(date, "yyyy-MM-dd");
-  const userData = getUserConnected();
-
-  const reservationDetails = {
-    userId: userData.id,
-    workStation: { id: stationId },
-    reservationDate: formattedDate,
-    morning: selectedOptions.timePeriod.morning,
-    afternoon: selectedOptions.timePeriod.afternoon,
-    reservationTypeId: selectedOptions.reservationType,
-  };
-
+export const bookWorkStation = async (reservationDetails, isGroupBooking) => {
   const jwt = sessionStorage.getItem(AUTH_TOKEN_KEY);
-
   if (!jwt) {
     console.error("No authentication token found.");
     return { success: false, message: "Authentication required." };
   }
 
+  let url = isGroupBooking ? "/api/reservations/addGroupReservation" : "/api/reservations/addIndividualReservation";
+  // let data = isGroupBooking ? { reservationDetails } : reservationDetails;
+  console.log("URL used: ", url);
+  console.log("Data sent to the server: ", JSON.stringify(reservationDetails, null, 2));
+
   try {
-    const response = await axios.post(
-      "/api/reservations/addReservation",
-      reservationDetails,
-      {
-        headers: {
-          Authorization: `Bearer ${jwt}`,
-        },
-      },
-    );
-    console.log("ResponseData from Service: ", response.data);
+    const response = await axios.post(url, reservationDetails, {
+      headers: { Authorization: `Bearer ${jwt}` }
+    });
+    console.log("Server response: ", response.data);
     return { success: true, details: response.data };
   } catch (error) {
-    console.error("Booking error :", error);
-    return { success: false, message: "Failed to book work station." };
+    console.error("Booking error: ", error);
+    console.error("Error details: ", error.response ? error.response.data : "No error data");
+    return { success: false, message: error.response ? error.response.data.message : "Failed to book work station." };
   }
 };
+
+
