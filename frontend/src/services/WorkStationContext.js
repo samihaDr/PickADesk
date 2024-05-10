6f7f92f1c6cbc6ce02abd4c7ee67d16c28c38e2f
SamihaD <samiha.dr@gmail.com>
2024-05-10 21:21:47 +0200

    Feat(ColleagueBooking : WorkStationContext): add isColleagueBooking ans selectedColleague variables to context

--- frontend/src/services/WorkStationContext.js
+++ frontend/src/services/WorkStationContext.js
@@ -3,39 +3,38 @@ import React, { createContext, useState } from "react";
 export const WorkStationContext = createContext();
 
 export const WorkStationProvider = ({ children }) => {
-  const [workStations, setWorkStations] = useState([]);
-  const [selectedOptions, setSelectedOptions] = useState({
-    date: new Date(), // une date par défaut
-    timePeriod: { morning: false, afternoon: false }, // valeurs par défaut
-    zone: "",
-    reservationType: "",
-    workArea: "",
-    // screen: "",
-    // equipment: [],
-    // furniture: [],
-  });
+    const [workStations, setWorkStations] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState({
+        date: new Date(), // une date par défaut
+        timePeriod: { morning: false, afternoon: false }, // valeurs par défaut
+        zone: "",
+        reservationType: "",
+        workArea: "",
+        // screen: "",
+        // equipment: [],
+        // furniture: [],
+    });
     const [isGroupBooking, setIsGroupBooking] = useState(false);
     const [teamMembers, setTeamMembers ] = useState([]);
     const [selectedStations, setSelectedStations ] = useState([]);
     const [selectedMembers, setSelectedMembers] = useState([]);
-  return (
-    <WorkStationContext.Provider
-      value={{
-        workStations,
-        setWorkStations,
-        selectedOptions,
-        setSelectedOptions,
-        isGroupBooking,
-        setIsGroupBooking,
-        teamMembers,
-        setTeamMembers,
-        selectedStations,
-        setSelectedStations,
-        selectedMembers,
-        setSelectedMembers
-      }}
-    >
-      {children}
-    </WorkStationContext.Provider>
-  );
+    const [isColleagueBooking, setColleagueBooking] = useState(false);
+    const [selectedColleague, setSelectedColleague] = useState(null);
+    return (
+        <WorkStationContext.Provider
+            value={{
+                workStations, setWorkStations,
+                selectedOptions, setSelectedOptions,
+                isGroupBooking, setIsGroupBooking,
+                teamMembers, setTeamMembers,
+                selectedStations, setSelectedStations,
+                selectedMembers, setSelectedMembers,
+                isColleagueBooking, setColleagueBooking,
+                selectedColleague, setSelectedColleague,
+
+            }}
+        >
+            {children}
+        </WorkStationContext.Provider>
+    );
 };
