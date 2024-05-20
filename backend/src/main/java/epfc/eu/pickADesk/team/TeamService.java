3fc761a9eb9132a6bcc47f90807ff8049667fd3e
SamihaD <samiha.dr@gmail.com>
2024-05-20 19:14:08 +0200

    Feat(EditTeamParameters/TeamService) : add and implement getAllTeams method to retrieve the team list and updateTeamQuota to apdate the team quotas

--- backend/src/main/java/epfc/eu/pickADesk/team/TeamService.java
+++ backend/src/main/java/epfc/eu/pickADesk/team/TeamService.java
@@ -1,6 +1,9 @@
 package epfc.eu.pickADesk.team;
 
+import epfc.eu.pickADesk.dto.TeamQuotaUpdateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -8,6 +11,7 @@ import java.util.List;
 public class TeamService {
     private final TeamRepository teamRepository;
 
+    @Autowired
     public TeamService(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
@@ -15,9 +19,26 @@ public class TeamService {
     //recuperer la liste des teams
     public List<Team> getTeamsByDepartment(Integer departmentId) {
         return teamRepository.findTeamsByDepartmentId(departmentId);
+
     }
 
     public Team getTeamById(Integer teamId) {
         return teamRepository.findTeamById(teamId);
+    }
+
+    public List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+
+    @Transactional
+    public TeamQuotaUpdateDTO updateTeamQuota(Integer teamId, TeamQuotaUpdateDTO quotaUpdateDTO) {
+        Team team = teamRepository.findById(teamId)
+                .orElseThrow(() -> new RuntimeException("Team not found"));
+
+        team.setTeamQuotaMax(quotaUpdateDTO.getTeamQuotaMax());
+        team.setTeamQuotaMin(quotaUpdateDTO.getTeamQuotaMin());
+
+        teamRepository.save(team);
+        return quotaUpdateDTO;
     }
 }
