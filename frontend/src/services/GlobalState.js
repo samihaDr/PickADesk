3a01e7b1c22ed4da03b5c381ce6493d2b4f69e4e
SamihaD <samiha.dr@gmail.com>
2024-05-22 23:15:41 +0200

    Refactor(GlobalState) : set the value of the WeeklyBooking variable at 0 in GlobalState

--- frontend/src/services/GlobalState.js
+++ frontend/src/services/GlobalState.js
@@ -8,7 +8,7 @@ const initialState = {
   userInfo: null,
   userPreferences: null,
   weeklyRemaining: null,
-  weeklyBookings: null,
+  weeklyBookings: 0,
   reservations: [],
   favorites: [],
   isAuthenticated: !!sessionStorage.getItem(AUTH_TOKEN_KEY),
