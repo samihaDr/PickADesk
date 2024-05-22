60ab97b33343b2358d1a1c55059ccf8928c59a65
SamihaD <samiha.dr@gmail.com>
2024-05-22 22:29:38 +0200

    Refactor(OfficeMap) : adjust image size and clean code

--- frontend/src/components/officeMap/OfficeMap.js
+++ frontend/src/components/officeMap/OfficeMap.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { MapContainer, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-
 const AddImageOverlay = ({ imageUrl, bounds }) => {
   const map = useMap();
 
-  // Utilisez React.useEffect pour ajouter l'overlay après que la carte a été initialisée
-  React.useEffect(() => {
+  useEffect(() => {
     const overlay = L.imageOverlay(imageUrl, bounds, { opacity: 1 }).addTo(map);
     return () => {
       map.removeLayer(overlay); // Nettoyez en enlevant l'overlay lorsque le composant est démonté
@@ -25,18 +23,17 @@ const OfficeMap = () => {
   ];
 
   return (
-    <div className="main">
-      <h2>Pick a desk</h2>
+
       <MapContainer
         center={[50, 50]} // Centrez la carte au milieu de l'image
         zoom={3} // Niveau de zoom initial
-        style={{ height: "100vh", width: "50%" }} // Hauteur et largeur de la carte
+        style={{ height: "80vh", width: "100%" }} // Hauteur et largeur de la carte
         crs={L.CRS.Simple} // Utilisation du système de coordonnées simplifié
       >
         <AddImageOverlay imageUrl={imageUrl} bounds={bounds} />
         {/* Pas besoin de TileLayer ici puisque vous n'utilisez pas de tuiles de carte géographique */}
       </MapContainer>
-    </div>
+
   );
 };
 
